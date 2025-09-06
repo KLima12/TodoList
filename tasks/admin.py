76ae240fdea6a79ab4258343995c1b38fcd706eb
KLima12 <143836141+KLima12@users.noreply.github.com>
2025-09-06 12:00:09 -0300

    Delete tasks directory

--- tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "deadline_date",
-                    "creation_date", "status")
--- tasks/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.view_all_tasks),
-    path('view_tasks/', views.view_tasks, name="view_tasks"),
-]
